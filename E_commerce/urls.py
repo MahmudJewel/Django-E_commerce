cc345328ddd520ecb5f8ec4e3008b03f4494ec1e
villain <villain@kali.local>
2021-09-04 03:04:41 +0600

    navbar is added

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include ('home.urls')),
-    path('admn',include ('admn.urls')),
-    path('customer',include ('customer.urls')),
+    path('admn/',include ('admn.urls')),
+    path('customer/',include ('customer.urls')),
 ]
--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from home import views
 
 urlpatterns = [
-    #path('',include ('home.urls')),
+    path('',views.home),
     
 ]
