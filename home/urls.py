0452c888fc1cf6eb64853f79f987118dbdb40368
villain <villain@kali.local>
2021-09-05 12:48:46 +0600

    login form created

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
+
 from home import views
+
 
 urlpatterns = [
     path('',views.home),
+    path('login', LoginView.as_view(template_name='home/login.html'), name='login'),
+
+
 ]
