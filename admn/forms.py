b7f447a7684a664a450de8f0bc1df4a7c2bad683
jewel <jmbengal.mbstu@gmail.com>
2021-12-15 22:06:58 +0600

    edited for rich text

--- admn/forms.py
+++ admn/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 from admn import models
+from tinymce.widgets import TinyMCE #tinymce rich text
 
 class productForm(forms.ModelForm):
+	# desc = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}))
 	class Meta:
 		model=models.product
 		fields=["productCategory", "name", "price", "desc", "img"]
+		# widgets = {
+  #           'desc': forms.CharField(widget=TinyMCE()),
+  #       }
 
 class categoryForm(forms.ModelForm):
 	class Meta:
