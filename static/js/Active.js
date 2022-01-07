156b5c7437244d364afe65d4754354ecf21229ce
jewel <jmbengal.mbstu@gmail.com>
2022-01-07 18:03:27 +0600

    product added in ladies category

--- static/js/Active.js
+++ static/js/Active.js
@@ -1,9 +1,9 @@
 console.log("Hello!!!, js connected");
 
 // Add active class to the current button (highlight it)
-let header = document.getElementById("subIndex");
-let btns = header.getElementsByClassName("abc");
-console.log(btns)
+// let header = document.getElementById("subIndex");
+// let btns = header.getElementsByClassName("abc");
+// console.log(btns)
 // for (let i = 0; i < btns.length; i++) {
 //   btns[i].addEventListener("click", function () {
 //     let current = document.getElementsByClassName("active");
@@ -17,3 +17,17 @@ console.log(btns)
 //         $(this).addClass("active").siblings().removeClass("active");
 //       });
 //   }
+// Add active class to the current button (highlight it)
+var hd = document.getElementById("subIndex");
+var btnss = hd.getElementsByClassName("btn");
+for (var i = 0; i < btnss.length; i++) {
+  btnss[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    if (current.length>0){
+        current[0].className = current[0].className.replace(" active", "");
+    }
+    // current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    console.log(this.className);
+  });
+}
