b4cef7536eb36a792a765140b1a4eb3b3ecd73ae
Jewel Mahmud <35461355+MahmudJewel@users.noreply.github.com>
2022-07-03 13:34:24 +0600

    Delete admn directory

--- admn/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from admn import models
-
-# Register your models here.
-admin.site.register(models.product)
-admin.site.register(models.product_category)
--- admn/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from admn import models
-
-class productForm(forms.ModelForm):
-	class Meta:
-		model=models.product
-		fields=["name", "price", "desc", "img"]
